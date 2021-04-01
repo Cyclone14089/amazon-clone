a9d510a3c8277f77e7ace64ccb7dda5f9c0ab64a
Bhargav Das Gupta <dasguptabhargav@gmail.com>
2021-04-01 16:18:55 +0530

    router introduced

--- src/App.js
+++ src/App.js
@@ -2,12 +2,27 @@ import React from "react";
 import Header from "./Header";
 import Home from "./Home";
 
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
 function App() {
   return (
-    <div className="App">
-      <Header />
-      <Home />
-    </div>
+    <Router>
+      <div className="App">
+        <Switch>
+
+          <Route path='/checkout'>
+            <Header />
+            <h1>CHECKOUT PAGE</h1>
+          </Route>
+
+          <Route path='/'>
+            <Header />
+            <Home />
+          </Route>
+
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
