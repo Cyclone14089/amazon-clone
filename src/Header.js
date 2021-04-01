8b9bb1ac8e8499fcbaf33a33d3c9aae15393d79a
Bhargav Das Gupta <dasguptabhargav@gmail.com>
2021-04-02 00:24:38 +0530

    adding links to header

--- src/Header.js
+++ src/Header.js
@@ -2,15 +2,19 @@ import React from 'react';
 import './Header.css';
 import SearchIcon from "@material-ui/icons/Search";
 import ShoppingBasketIcon from '@material-ui/icons/ShoppingBasket';
+import { Link } from 'react-router-dom';
 
 function Header() {
+
     return (
         <div className='header'>
 
-            <img
-                className='header_logo'
-                src="http://pngimg.com/uploads/amazon/amazon_PNG11.png"
-            />
+            <Link to='/'>
+                <img
+                    className='header_logo'
+                    src="http://pngimg.com/uploads/amazon/amazon_PNG11.png"
+                />
+            </Link>
 
             <div className='header_search'>
                 <input
@@ -37,10 +41,12 @@ function Header() {
                     <span className="header_optionLineTwo">Prime</span>
                 </div>
 
-                <div className="header_optionBasket">
-                    <ShoppingBasketIcon />
-                    <span className="header_optionLineTwo header_basketCount">0</span>
-                </div>
+                <Link to='/checkout'>
+                    <div className="header_optionBasket">
+                        <ShoppingBasketIcon />
+                        <span className="header_optionLineTwo header_basketCount">0</span>
+                    </div>
+                </Link>
 
             </div>
 
